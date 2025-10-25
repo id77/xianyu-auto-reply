d9e2bf2a9f3b34d0caf320bf04d338b0fd45f72c
id77 <9592236+id77@users.noreply.github.com>
2025-10-25 10:23:25 +0800

    Update XianyuAutoAsync.py

--- XianyuAutoAsync.py
+++ XianyuAutoAsync.py
@@ -5216,6 +5216,8 @@ class XianyuLive:
                 args=browser_args
             )
 
+            headers = DEFAULT_HEADERS.copy()
+
             # 创建浏览器上下文
             context_options = {
                 'user_agent': headers['user-agent'] or 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36'
